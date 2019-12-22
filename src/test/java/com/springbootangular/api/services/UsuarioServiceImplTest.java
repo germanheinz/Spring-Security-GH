66ff51375e073774a2850695852b659f6fdd85a8
germanheinz <heinz.german@gmail.com>
2019-12-22 18:13:34 +0100

    Test UserServiceImpl - loadUserByUsername -

--- src/test/java/com/springbootangular/api/services/UsuarioServiceImplTest.java
+++ src/test/java/com/springbootangular/api/services/UsuarioServiceImplTest.java
@@ -2,6 +2,7 @@ package com.springbootangular.api.services;
 
 import api.v1.mapper.UsuarioMapper;
 import api.v1.model.UsuarioDTO;
+import com.springbootangular.api.domain.Role;
 import com.springbootangular.api.domain.Usuario;
 import com.springbootangular.api.repositories.UsuarioRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,10 +10,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+
+// Testing UsuarioService
 class UsuarioServiceImplTest {
 
     public static final Long ID = 1L;
@@ -42,4 +48,26 @@ class UsuarioServiceImplTest {
         assertEquals(ID, usuario.getId());
         assertEquals(NAME, usuario.getUsername());
     }
+
+    @Test
+    void loadUserByUsername() {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setUsername("GermanHeinz");
+        usuario.setNombre("German");
+
+        Role role = new Role();
+        role.setNombre("ROLE_ADMIN");
+        role.setNombre("ROLE_USER");
+
+        List<Role> listaRoles = new ArrayList<Role>();
+        listaRoles.add(role);
+
+        usuario.setRoles(listaRoles);
+
+        when(usuarioRepository.findByUsername(anyString())).thenReturn(usuario);
+
+        assertEquals(ID, usuario.getId());
+    }
+
 }
